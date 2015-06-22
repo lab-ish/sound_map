361b921b1d855fcb5dfaa62f693fa773a55b04ac
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2015-06-22 13:08:49 +0900

    Save GCC-PHAT result.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 #
 
 import sys
+import numpy as np
 
 import wave_data
 import signal_process
@@ -23,5 +24,15 @@ if __name__ == '__main__':
 
     # データ処理
     sig = signal_process.SignalProcess(data.raw_data[0], data.raw_data[1])
+    sound_map = sig()
+
+    # ファイルへの書き出し
+    index = np.arange(0, len(sound_map))
+    timebox = data.sample_timelen * sig.shift * index
+    sound_maptime = sound_map * data.sample_timelen
+    save_data = np.c_[index, timebox, sound_map, sound_maptime]
+    np.savetxt("sound_map.dat", save_data,
+               fmt=["%d", "%g", "%g", "%g"],
+               delimiter="\t")
 
     del data
