227eee5796c47ae712097504b57d63f0e419818f
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2017-06-21 07:53:22 +0900

    Temporarily finished working on PCA module.

--- noise_reduction.py
+++ noise_reduction.py
@@ -27,13 +27,29 @@ class NoiseReduction():
         return
 
     #--------------------------------------------------
+    def pca_apply(self, data):
+        # 単一データの場合は変換して渡す
+        if len(data.shape) == 1:
+            return self.pca.transform(data.reshape(1,-1))
+        # そうでない場合はそのまま渡す
+        return self.pca.transform(data)
+
+    #--------------------------------------------------
+    def pca_load(self, pca_file):
+        self.pca = joblib.load(pca_file)
+        self.n_comp = self.pca.n_components_
+        return
+
+    #--------------------------------------------------
     def pca_train(self, data, savefile=None):
         self.pca = PCA(n_components=self.n_comp)
         end = self.data.shape[0]-self.folds
         # FFT結果の格納先
         self.fft_data = np.zeros(self.winsize/16*end).reshape(end,self.winsize/16)
-        for cnt in range(end):
-            self.fft_data[cnt,:] = self.fft(data, cnt)
+
+        for offset in range(end):
+            # データは折り返してあるので、必要行数を取り出してから1行に変換してFFT
+            self.fft_data[offset,:] = self.fft(data[offset:offset+self.folds].reshape(1,-1)[0])
 
         # PCA
         transformed = self.pca.fit_transform(self.fft_data)
@@ -44,13 +60,12 @@ class NoiseReduction():
         return transformed
 
     #--------------------------------------------------
-    def fft(self, data, offset=0):
+    def fft(self, data):
         # FFT window
         win = np.hamming(self.winsize)
 
         # ウィンドウをかける
-        #   データは折り返してあるので、必要行数を取り出してから1行に変換する
-        win_data = win * (data[offset:offset+self.folds].reshape(1,-1)[0])
+        win_data = win * data
 
         # FFT
         #   実信号の複素FFTでは後半の半分は折り返しなので捨てる
