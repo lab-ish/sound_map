b706646f8473d30f1bf8f1db043ce5d93e308af1
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2018-07-30 10:56:54 +0900

    Remove unnecessary codes.

--- eval_analysis.py
+++ eval_analysis.py
@@ -118,31 +118,3 @@ if __name__ == '__main__':
         fp["start"] = fp.time - args.range/2
         fp["end"]   = fp.time + args.range/2
         fp.apply(plot_time, axis=1, args=[args.fp_plot])
-
-    # fndata["start"] = fndata.true_pass - args.range/2
-    # fndata["end"]   = fndata.true_pass + args.range/2
-    # start_end = np.array(fndata[["start", "end"]])
-
-    # for i in range(start_end.shape[0]):
-    #     start = start_end[i,0]
-    #     end   = start_end[i,1]
-    #     plotout = args.plotbase + "fn_%.2f.eps" % (start + args.range/2)
-
-    #     fig = plt.figure()
-    #     #------------------------------
-    #     plt.rcParams['font.family'] = 'Times New Roman' # 全体のフォント
-    #     plt.rcParams['font.size'] = 18                # フォントサイズ
-    #     plt.rcParams['axes.linewidth'] = 1            # 軸の太さ
-    #     fig.subplots_adjust(bottom = 0.15)            # 下の余白を増やす
-    #     # EPS出力のためのおまじない
-    #     plt.rcParams['ps.useafm'] = True
-    #     plt.rcParams['pdf.use14corefonts'] = True
-    #     plt.rcParams['text.usetex'] = True
-    #     #------------------------------
-    #     plt.xlim([start,end])
-    #     plt.ylim([-1.55,1.55])
-    #     plt.xlabel("Time [s]")
-    #     plt.ylabel("Sound Delay [ms]")
-    #     plt.plot(soundmap[:,0], soundmap[:,1], '.')
-    #     plt.savefig(plotout)
-    #     plt.close()
